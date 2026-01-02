c40b3772e6f3ce891fdbcd9fc1bc15524962b996
akashkuppattil-dev <116348341+akashkuppattil-dev@users.noreply.github.com>
2026-01-02 09:29:09 +0530

    Prepare project for Vercel deployment

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
-import express, { type Request, Response, NextFunction } from "express";
+import express, { NextFunction, type Request, Response } from "express";
+import { createServer } from "http";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
-import { createServer } from "http";
 
 const app = express();
 const httpServer = createServer(app);
@@ -84,15 +84,10 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.listen(
-    {
-      port,
-      host: "0.0.0.0",
-      reusePort: true,
-    },
-    () => {
-      log(`serving on port ${port}`);
-    },
-  );
+
+
+
+export default app;
+
+
 })();
